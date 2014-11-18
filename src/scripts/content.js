d57083166c071c119d7f6170c09926645745c95c
Paul Brzeski <mail@paulbrzeski.com>
2014-11-19 01:52:32 +0800

    Working on content code

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -1,13 +1,24 @@
 $(document).ready(function(){
 	$('.ui.accordion').accordion();
 	$('.ui.sidebar').sidebar('show');
-	$.ajax(
-		{	
-			dataType: "jsonp",
-			url:"https://api.github.com/repos/paulbrzeski/Langenium/contents/",
-			success: function(data){
-				console.log(data)
-			}
-			
-		});	
+
+	init();
+	
 });
+
+function init() {
+	$.ajax({	
+		dataType: "jsonp",
+		url:"https://api.github.com/repos/paulbrzeski/paulbrzeski.github.io/contents/src/content/archive/2014/11/1-1.blob",
+		success: function(data){
+			var d = data.data.content.replace('\n','');
+			var raw_blob = Base64.decode(d);
+			raw_blob = raw_blob.split(/^[^\}\)]+-[^\}\)]+$/);
+
+			console.log(raw_blob)
+			var blob_meta = eval(raw_blob[0]);
+			var blob_data = markdown(raw_blob[1])
+			
+		}
+	});	
+}
